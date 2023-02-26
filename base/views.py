4f26eb6131fff63ef2dae1857866bd197aa2cdc2
aryanaruni <lordaryan20@gmail.com>
2023-02-26 22:51:10 +0530

    readme udated

--- base/views.py
+++ base/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
-    return HttpResponse('Home Page')
+    # return HttpResponse('Home Page')
+    return render(request , 'home.html')
 
 def Rooms(request):
-    return HttpResponse('Rooms')
+    # return HttpResponse('Rooms')
+    return render(request , 'rooms.html')
 
 class Test():
     def render_test(request):
         return HttpResponse('Test')
+        # return render(request , 'home.html')
